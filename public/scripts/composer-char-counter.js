e0b7cc60df54fd06736bfb23b9afbba493908a95
Don-Stevenson <dsteven1@gmail.com>
2019-12-17 19:42:20 +0000

    add character count function / styling

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,13 @@
+
+$(document).ready(() => {
+  $(".new-tweet textarea").on('keyup', function () {    
+    const remainingChar = 140 - $(this).val().length;
+    const counter = $(this).closest(".new-tweet").find(".counter");
+    if (remainingChar < 0){
+      counter.addClass("changeCounterColor");
+    } else {
+      counter.removeClass("changeCounterColor");
+    }    
+    counter.text(remainingChar);
+  });
+});
