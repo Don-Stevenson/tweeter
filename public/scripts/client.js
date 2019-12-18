e745b5251e9218694e8dfa34eb398a623ce4c671
Don-Stevenson <dsteven1@gmail.com>
2019-12-18 01:41:54 +0000

    fix extra border and picture location

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,8 +1,6 @@
 /* Reminder: Use (and do all your DOM work in) jQuery's document ready function
 */
 
-
-$(document).ready(function() {
   const data = [
     {
     "user": {
@@ -13,7 +11,7 @@ $(document).ready(function() {
     "content": {
         "text": "If I have seen further it is by standing on the shoulders of giants"
       },
-    "created_at": 1461116232227
+    "created_at": "10 days ago"
     },
     {
       "user": {
@@ -23,7 +21,7 @@ $(document).ready(function() {
       "content": {
         "text": "Je pense , donc je suis"
       },
-      "created_at": 1461113959088
+      "created_at": "12 days ago"
     }
   ]
 
@@ -59,5 +57,6 @@ $(document).ready(function() {
     }  
   }
   
-  renderTweets(data);
+  $(document).ready(function() {
+    renderTweets(data);
   });
