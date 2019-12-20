765843488f6bf42105359bd26af8931fb58e1b81
Don-Stevenson <dsteven1@gmail.com>
2019-12-20 01:48:14 +0000

    minor css tweek, add docs directory with pictures

--- README.md
+++ README.md
@@ -2,7 +2,21 @@
 
 Tweeter is a simple, single-page Twitter clone.
 
+## Learning Objectives
+
 This repository is the starter code for the project: Students will fork and clone this repository, then build upon it to practice their HTML, CSS, JS, jQuery and AJAX front-end skills, and their Node, Express and MongoDB back-end skills.
+
+## Overview 
+
+This web based application allows users to make a tweet which will send the data to the back end server.
+
+Upon success, client will get the values back from the tweet database in the server and render the tweets back onto the client's page.
+
+This application supports both mobile and windows view based on screen resolution.
+
+## Final Product Screen Shots
+!["screenshot of desktop view on load"]()
+
 
 ## Getting Started
 
@@ -10,8 +24,12 @@ This repository is the starter code for the project: Students will fork and clon
 2. Install dependencies using the `npm install` command.
 3. Start the web server using the `npm run local` command. The app will be served at <http://localhost:8080/>.
 4. Go to <http://localhost:8080/> in your browser.
+5. Check out the cool features such as making tweets by initially clicking on "Write a new tweet" message on top right corner and auto scroll up function when window is scrolled down!
+6. Enjoy!
+
 
 ## Dependencies
 
 - Express
 - Node 5.10.x or above
+- Body-Parser 1.15.2 or above
--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,20 +4,35 @@
 
 
 // escape function to prevent XSS attacks
-const escape = function (str) {
+const escape = function(str) {
   let div = document.createElement('div');
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
-}
+};
 
-// hidding the elment on load up
-
-// //function to convert unix timestamp to date
-// (1970,1,1))*86400 
+// //function to convert unix timestamp to regular date in years, days, hours, etc.
+formatTweetTimeStamp = (time) => {
+  const timeAgo = new Date().getTime() - time;
+  const secs = timeAgo / 1000;
+  const mins = secs / 60;
+  const hours = mins / 60;
+  const days = hours / 24;
+  const years = days / 365;
+  
+  if (years > 1) {
+    return Math.floor(years) + " year(s) ago";
+  } else if (days > 1) {
+    return Math.floor(days) + " day(s) ago";
+  } else if (hours > 1) {
+    return Math.floor(hours) + " hour(s) ago";
+  } else if (mins > 1) {
+    return Math.floor(mins) + " minute(s) ago";
+  } else return Math.floor(secs) + " second(s) ago";
+};
 
 
 // function that creates the tweet using html
-const createTweetElement = function (tweet) {
+const createTweetElement = function(tweet) {
   let $tweet = $('<article>').addClass('tweet');
   const htmlCode = `
   <header>
@@ -33,7 +48,7 @@ const createTweetElement = function (tweet) {
   </header>
   <p>${escape(tweet.content.text)}</p>
   <footer>
-  <p>${tweet.created_at}
+  <p>${formatTweetTimeStamp(tweet.created_at)}
   <span>                
   <i class="fa fa-flag">&#160</i>
   <i class="fa fa-retweet">&#160</i>
@@ -44,42 +59,42 @@ const createTweetElement = function (tweet) {
   `;
   $tweet.append(htmlCode);
   return $tweet;
-}
+};
 // function that renders the tweets on the page
-const renderTweets = function (tweets) {
+const renderTweets = function(tweets) {
   for (let tweet of tweets) {
     $('#tweets-container').prepend(createTweetElement(tweet));
   }
-}
+};
 // upon loading the the page, render the previous tweets
-$(document).ready(function () {
-  const loadTweets = function () {
+$(document).ready(function() {
+  const loadTweets = function() {
     $.ajax({
       url: "/tweets",
       dataType: "json",
       type: "GET",
-      success: function (tweetDB) {
+      success: function(tweetDB) {
         renderTweets(tweetDB);
       },
-      error: function () {
+      error: function() {
         $("#error").text("Error with ajax request");
         $("#error").slideDown();
         event.preventDefault();
       }
     });
-  }
+  };
   $("#error").hide();
   loadTweets();
 
   //after loading the page, handling the submit function
-  $("#submit").submit(function (event) {
+  $("#submit").submit(function(event) {
     noOfChars = $(this).find("textarea").val().length;
     if (noOfChars === 0) {
       $("#error").text("Invalid entry: Please enter your text below");
       $("#error").slideDown();
-      event.preventDefault()
+      event.preventDefault();
     } else if (noOfChars > 140) {
-      $("#error").text("Invalid entry: The maxium character length has been exceeded")
+      $("#error").text("Invalid entry: The maxium character length has been exceeded");
       $("#error").slideDown();
       event.preventDefault();
     } else {
@@ -90,11 +105,11 @@ $(document).ready(function () {
         type: "POST",
         contentType: "application/x-www-form-urlencoded",
         data: $(this).find("textarea").serialize(),
-        success: function () {
+        success: function() {
           loadTweets();
           $("#error").slideUp();
         },
-        error: function () {
+        error: function() {
           $("#error").text("Error with ajax request");
           $("#error").slideDown();
           event.preventDefault();
@@ -106,7 +121,7 @@ $(document).ready(function () {
   });
 
   // handling the toggle of the compose tweet bar
-  $("#write-tweet").on("click", function () {
+  $("#write-tweet").on("click", function() {
     if ($(".new-tweet:hidden").length) {
       $(".new-tweet").slideDown();
       $(".new-tweet").find("textarea").focus();
