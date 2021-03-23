4169e50f998a37851b754e37be05febea5e3eaec
Don-Stevenson <dsteven1@gmail.com>
2021-03-23 16:16:01 -0400

    update formatting

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,6 @@
 // function that counts down the remaining characters in your tweet turns red on negative values
 $(document).ready(() => {
-  $(".new-tweet textarea").on('input', function() {
+  $(".new-tweet textarea").on("input", function () {
     const remainingChar = 140 - $(this).val().length;
     const counter = $(this).closest(".new-tweet").find("#counter");
     if (remainingChar < 0) {
