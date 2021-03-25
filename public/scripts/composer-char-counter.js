2cf7c8fe8fa8fd14c9da51f3d9c2ae57e873be93
Don-Stevenson <dsteven1@gmail.com>
2021-03-24 21:53:44 -0400

    refactor and make a function to change counter text colour. add comments

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,17 +1,28 @@
-// function that counts down the remaining characters in your tweet turns red on negative values
 $(document).ready(() => {
   $(".new-tweet textarea").on("input", function() {
+    // calculating the remaining characters left
     const remainingChar = 140 - $(this).val().length;
+
+    // storing the #counter element from the page in a variable
     const counter = $(this).closest(".new-tweet").find("#counter");
-    if (remainingChar < 0) {
-      counter.removeClass("changeCounterYellow");
-      counter.addClass("changeCounterRed");
-    } else if (remainingChar >= 0 && remainingChar <= 20) {
-      counter.addClass("changeCounterYellow");
-    } else {
-      counter.removeClass("changeCounterRed");
-      counter.removeClass("changeCounterYellow");
-    }
+
+    // calling the change class colour based on remaining characters
+    changeClassColourByCharacter(remainingChar, counter);
+
+    // updating the counter text to be the remaining characters left
     counter.text(remainingChar);
   });
 });
+
+// function that turns the colour of the counter text by the number of remaining characters
+const changeClassColourByCharacter = (remainingChar, counter) => {
+  if (remainingChar < 0) {
+    counter.removeClass("changeCounterYellow");
+    counter.addClass("changeCounterRed");
+  } else if (remainingChar >= 0 && remainingChar <= 20) {
+    counter.addClass("changeCounterYellow");
+  } else {
+    counter.removeClass("changeCounterRed");
+    counter.removeClass("changeCounterYellow");
+  }
+};
