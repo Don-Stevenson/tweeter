81c5e06bed7a45be71c9926f71c93e6c29a6e75c
Don-Stevenson <dsteven1@gmail.com>
2021-03-24 21:32:17 -0400

    add a yellow class when remaining chars are less than 20

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,17 @@
 // function that counts down the remaining characters in your tweet turns red on negative values
 $(document).ready(() => {
-  $(".new-tweet textarea").on('input', function() {
+  $(".new-tweet textarea").on("input", function () {
     const remainingChar = 140 - $(this).val().length;
+    console.log({ remainingChar });
     const counter = $(this).closest(".new-tweet").find("#counter");
     if (remainingChar < 0) {
-      counter.addClass("changeCounterColor");
+      counter.removeClass("changeCounterYellow");
+      counter.addClass("changeCounterRed");
+    } else if (remainingChar >= 0 && remainingChar <= 20) {
+      counter.addClass("changeCounterYellow");
     } else {
-      counter.removeClass("changeCounterColor");
+      counter.removeClass("changeCounterRed");
+      counter.removeClass("changeCounterYellow");
     }
     counter.text(remainingChar);
   });
